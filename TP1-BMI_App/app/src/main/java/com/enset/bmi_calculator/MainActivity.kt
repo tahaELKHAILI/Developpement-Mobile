fd7b8a79bc5de192c55f872ea3358484061333a1
tahaELKHAILI <tahaelkhaili@gmail.com>
2026-06-22 22:06:45 +0100

    Error handling + readme

--- TP1-BMI_App/app/src/main/java/com/enset/bmi_calculator/MainActivity.kt
+++ TP1-BMI_App/app/src/main/java/com/enset/bmi_calculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.enset.bmi_calculator
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -23,34 +24,44 @@ class MainActivity : AppCompatActivity() {
 
         //Logic
         submit_button.setOnClickListener { view ->
-            val poids = poids_field.text.toString().toDouble();
-            val taille = taille_field.text.toString().toDouble();
-            //bmi poids en kg + taille en m
-            val bmi = poids/(taille/100).pow(2)
 
-            if(bmi < 18.5){
-                result_text_field.text = "Votre IMC est: ${bmi}"
-                result_image_field.setImageResource(R.drawable.maigre)
+            if(poids_field.text.isEmpty() || taille_field.text.isEmpty()){
+                result_text_field.text = getString(R.string.invalid_input)
+                result_text_field.setTextColor(Color.RED)
+                result_image_field.setImageBitmap(null)
             }
-            else if(bmi <25){
-                result_text_field.text = "Votre IMC est: ${bmi}"
-                result_image_field.setImageResource(R.drawable.normal)
-            }
-            else if(bmi <30){
-                result_text_field.text = "Votre IMC est: ${bmi}"
-                result_image_field.setImageResource(R.drawable.obese)
-            }
-            else if(bmi <40){
-                result_text_field.text = "Votre IMC est: ${bmi}"
-                result_image_field.setImageResource(R.drawable.t_obese)
-            }
-            else {
-                result_text_field.text = "Votre IMC est: ${bmi}"
-                result_image_field.setImageResource(R.drawable.surpoids)
-            }
+            else{
+                result_text_field.setTextColor(Color.BLACK)
+                val poids = poids_field.text.toString().toDouble()
+                val taille = taille_field.text.toString().toDouble()
+                //bmi poids en kg + taille en m
+                val bmi = poids/(taille/100).pow(2)
 
-            poids_field.setText("")
-            taille_field.setText("")
+                if(bmi < 18.5){
+                    result_text_field.text = getString(R.string.resultats_text, bmi)
+                    result_image_field.setImageResource(R.drawable.maigre)
+                }
+                else if(bmi <25){
+                    result_text_field.text = getString(R.string.resultats_text, bmi)
+                    result_image_field.setImageResource(R.drawable.normal)
+                }
+                else if(bmi <30){
+                    result_text_field.text = getString(R.string.resultats_text, bmi)
+                    result_image_field.setImageResource(R.drawable.obese)
+                }
+                else if(bmi <40){
+                    result_text_field.text = getString(R.string.resultats_text, bmi)
+                    result_image_field.setImageResource(R.drawable.t_obese)
+                }
+                else {
+                    result_text_field.text = getString(R.string.resultats_text, bmi)
+                    result_image_field.setImageResource(R.drawable.surpoids)
+                }
+
+                poids_field.setText("")
+                taille_field.setText("")
+
+            }
 
         }
     }
